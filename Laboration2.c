b90074067e6ab3e196eb1d2312bdf7ee278e7859
Adrian S <89584348+GittyEvil@users.noreply.github.com>
2024-10-04 14:52:58 +0200

    Add files via upload

---
+++ Laboration2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+
+/*
+Krav i uppgift:
+tre st page replacement algorithms:
+    *FIFO
+    *OPT
+    *LRU
+när programmet körs så används:
+    *vmsim -a <fifo | optimal | lru> -n <number of frames> -f <trace file>
+    -n : number of frames in physical memory
+    -f : the name of the trace file with vr memory
+
+mer grejer som behövs:
+    page table (page table hit)
+    backing store (page fault)
+    page replacement (page out/page in)
+
+sist ska en summering skrivas ut
+
+Storlekar:
+    storlek på vr minne = 64Kb
+    16 bit adresser(0x0000-0xffff)
+    frame size 256 bytes
+    physical memory = <frames> x 256 bytes
+
+*/
+
+
+
+
+
+void main() {
+
+    //fifo
+    
+    //UPT
+    
+    //LRU
+
+
+    return 0;
+}
+
+
+
