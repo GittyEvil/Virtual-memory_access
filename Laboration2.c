b83bd35a07d71a36b8c9fc6057644778756c5ea4
Adrian S <89584348+GittyEvil@users.noreply.github.com>
2024-10-13 17:29:41 +0200

    läs desc
    fixar just nu med filhanteringen av .dat filerna å adresser för sen att börja på FIFO, OPT å LRU

--- Laboration2.c
+++ Laboration2.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+#define nbrFrames 256
 
 /*
 Krav i uppgift:
@@ -16,6 +18,7 @@ mer grejer som behövs:
     page table (page table hit)
     backing store (page fault)
     page replacement (page out/page in)
+    kunna läsa .dat filerna
 
 sist ska en summering skrivas ut
 
@@ -27,18 +30,100 @@ Storlekar:
 
 */
 
+/*
+filhantering
+read
+
+behöver kunna läsa av filerna, storlek å läsa in de rätt för hantering
+*/
 
 
+void fifo() {
 
+}
+
+void upt() {
+
+}
+
+void lru() {
+
+}
+//för att kunna läsa antalet adresser i filen som används
+int count_Lines(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Kunde inte öppna filen");
+        exit(1);
+    }
+
+    int count = 0;
+    char line[256]; 
+
+    // Läs filen rad för rad
+    while (fgets(line, sizeof(line), file) != NULL) {
+        count++;
+    }
+
+    fclose(file);
+    return count;
+}
+
+//för att kunna läsa .dat filer
+void readFile(char filename,) {
+    int capacityAdresses = 10;
+    int *adresses = (int*) malloc(capacity * sizeof(int));
+    FILE *file = fopen(filename,'r');
+
+    if(file == NULL) {
+        printf("kunde ej läsa fil");
+        exit(0);
+    }
+
+    if(adresses == NULL) {
+        printf("problem vid allokering");
+    }
+    
+    //hantering av filadresser
+    //konvertera om hexa till heltal
+    while(fscanf(file,"%x",%adress) == 1) {
+        if(nbrAdresses >= capacity) {
+            capacity *=2;
+            adresses = (int*)realloc(adresses, capacity * sizeof(int));
+        }
+        
+        if(adresses == NULL) {
+            printf("problem vid omvandlingen å allokering");
+            exit(0);
+        }
+
+        adresses[*nbrAdresses] = adress;
+        (*nbrAdresses)++;
+    }
+    
+    fclose();
+    return adresses;
+}
 
 void main() {
-
+    const char* filename;
+    scanf("%s",filename);
+    
     //fifo
     
     //UPT
     
     //LRU
 
+    //init simulering
+    
+    //läsning av fil å hantering av adresser
+    int nbrAdresses = count_Lines(filename);
+    int* adresses = readFile(filename,nbrAdresses);
+    
+    //simulering
+
+    //summering
 
     return 0;
 }
