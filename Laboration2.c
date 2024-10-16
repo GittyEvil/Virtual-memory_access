5c6b16b9191e33d5fea68cfc2a69f6cf3bfd5372
Adrian S <89584348+GittyEvil@users.noreply.github.com>
2024-10-16 11:40:09 +0200

    läs desc
    har nu börjat med intiering av simulatorn, insåg att jag inte kan börja med algoritmerna innan jag har grunden för simulatorn.

--- Laboration2.c
+++ Laboration2.c
@@ -3,6 +3,9 @@
 
 #define nbrFrames 256
 
+//Adrian Stude och Mohamad Muhra
+
+
 /*
 Krav i uppgift:
 tre st page replacement algorithms:
@@ -37,18 +40,80 @@ read
 behöver kunna läsa av filerna, storlek å läsa in de rätt för hantering
 */
 
+typedef struct {
+    int algo; //algoritmen du väljer i kommando
+    int nbrFrames; //antal frames
+    int pageTable;
+    int pageFaults; //missar
+    int pageHits; //träffar
+    int pageChange; //sido byte
+    int pMemory; //fysiskt minne
+}vsim;
+
+//ska initiera hela simuleringen
+vsim* create_sim(int algo, int nbrFrames) {
+    /*
+    ska 'nollställa' allt å initiera allt så när den faktiskt ska köras
+    så är det bara att välja specifikationer å köra(nästa funktion dvs)
+    */
+
+    vsim *sim = (vsim*) malloc(sizeof(vsim));
+    sim ->algo = algo;
+    sim ->nbrFrames = nbrFrames;
+    sim ->pageFaults = 0;
+    sim ->pageHits = 0;
+    sim ->pageChange = 0;
+
+    sim -> pageTable = (int*)malloc(sizeof(int) * nbrFrames);
+    sim -> pMemory = (int*)malloc(sizeof(int) * nbrFrames);
+
+    for(int i = 0; i < nbrFrames; i++) {
+        sim ->pMemory[i]= -1;
+    }
+    
+    return sim;
+}
+
+vsim simulation(vsim *sim, int adresses, int nbrAdresses) {
+    for(int i = 0; i < nbrFrames;i++) {
+        int page = adresses[i];
+    }
+
+    switch(sim -> algo) {
+        //fifo
+        case 0:
+            //fifo();
+            break;
+        //opt
+        case 1:
+            //upt();
+            break;
+        //lru
+        case 2:
+            //lru();
+            break;
+        
+    }
+}
 
 void fifo() {
-
+    /*
+    hantering av first in first out
+    */
 }
 
 void upt() {
-
+    /*
+    kräver att du kan se fil för att kunna lista ut vad som ska tas bort först
+    */
 }
 
 void lru() {
-
+    /*
+    least read ues time eller nått, senast läst nyligen bort först
+    */
 }
+
 //för att kunna läsa antalet adresser i filen som används
 int count_Lines(const char *filename) {
     FILE *file = fopen(filename, "r");
@@ -70,7 +135,7 @@ int count_Lines(const char *filename) {
 }
 
 //för att kunna läsa .dat filer
-void readFile(char filename,) {
+void readFile(char filename,int nbrAdresses) {
     int capacityAdresses = 10;
     int *adresses = (int*) malloc(capacity * sizeof(int));
     FILE *file = fopen(filename,'r');
@@ -96,13 +161,9 @@ void readFile(char filename,) {
             printf("problem vid omvandlingen å allokering");
             exit(0);
         }
-
-        adresses[*nbrAdresses] = adress;
-        (*nbrAdresses)++;
     }
     
     fclose();
-    return adresses;
 }
 
 void main() {
@@ -116,7 +177,6 @@ void main() {
     //LRU
 
     //init simulering
-    
     //läsning av fil å hantering av adresser
     int nbrAdresses = count_Lines(filename);
     int* adresses = readFile(filename,nbrAdresses);
